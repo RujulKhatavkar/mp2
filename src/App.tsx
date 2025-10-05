d5a6d4911e43d4fcd8c3adf12879714e4f435f25
RujulKhatavkar <81792910+RujulKhatavkar@users.noreply.github.com>
2025-10-05 16:56:41 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ListView from "./pages/ListView";
 import GalleryView from "./pages/GalleryView";
 import DetailView from "./pages/DetailView";
 
-const basename = process.env.NODE_ENV === "production" ? "/mp2" : "/";
+const basename = "/mp2";
 
 export default function App() {
   return (
